cb0495ed122b5ba7028601c5d3512b491ef17425
Egor.Vlasov <qwerty@gmail.com>
2026-07-11 18:12:20 +0300

    complete A2 Async-deadlock

---
+++ Block01/AsyncDeadlock/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+
+    private static async Task Main(string[] args)
+    {
+        string value = await GetData();
+        Console.WriteLine(value);
+    }
+    
+    private static async Task<string> GetData()
+    {
+        return await FetchAsync();
+    }
+    
+    private static async Task<string> FetchAsync()
+    {
+        await Task.Delay(100);
+        return "ok";
+    }
+}
+
+
